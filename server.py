ded3c9fed4be77b140e9a9de80b39af614a23c64
nandobfer <nandobfer@yahoo.com.br>
2022-09-05 17:35:15 -0300

    changed file name to timer.html

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('test.html')
+    return render_template('timer.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="5000")
