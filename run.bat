213c62f97e87c83a775a89af6923cd2faee8ed27
nandobfer <nandobfer@yahoo.com.br>
2022-09-12 15:35:30 -0300

    reorganized electron files

--- run.bat
+++
@@ -1,2 +0,0 @@
-start /B py -3.8 server.py
-start /B py -3.8 timesheet.py
--- timesheet.py
+++
@@ -1,6 +0,0 @@
-import webview
-
-webview.create_window('Timesheet', 'http://127.0.0.1:5000', text_select=True, resizable=False)
-
-if __name__ == '__main__':
-    webview.start(debug=True)
