8d1272a9074f989b16873822bc963469886371d5
nandobfer <nandobfer@yahoo.com.br>
2022-09-12 14:23:53 -0300

    fixed timer lag

--- static/javascript/timer.js
+++ static/javascript/timer.js
@@ -16,45 +16,120 @@ class Registro {
     }
 }
 
-let time = 1;
-let time2 = 0;
+class Timer {
+    constructor () {
+      this.isRunning = false;
+      this.startTime = 0;
+      this.overallTime = 0;
+    }
+  
+    _getTimeElapsedSinceLastStart () {
+      if (!this.startTime) {
+        return 0;
+      }
+    
+      return Date.now() - this.startTime;
+    }
+  
+    start () {
+      if (this.isRunning) {
+        return console.error('Timer is already running');
+      }
+  
+      this.isRunning = true;
+  
+      this.startTime = Date.now();
+    }
+  
+    stop () {
+      if (!this.isRunning) {
+        return console.error('Timer is already stopped');
+      }
+  
+      this.isRunning = false;
+  
+      this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+  
+    reset () {
+      this.overallTime = 0;
+  
+      if (this.isRunning) {
+        this.startTime = Date.now();
+        return;
+      }
+  
+      this.startTime = 0;
+    }
+  
+    getTime () {
+      if (!this.startTime) {
+        return 0;
+      }
+  
+      if (this.isRunning) {
+        return this.overallTime + this._getTimeElapsedSinceLastStart();
+      }
+  
+      return this.overallTime;
+    }
+  }
+  
+const timer = new Timer();
+  
+
 let clock = $('#clock');
 const timer_button = $('#timer-button');
 const input_field = $('#input-field');
-let intervalId;
 
-let minutes;
-let hour;
+let minutes = 0;
+let hour = 0;
 let seconds;
+let flag_seconds = false;
+let flag_minutes = false;
 
 let registros = [];
 
 const updateCount = () => {
-    hour = Math.floor(time/3600);
-    minutes = Math.floor(time2/60);
-    seconds = time % 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = Math.round(timer.getTime() / 1000)
+    seconds = seconds % 60;
+    if (seconds == 1) {
+        flag_seconds = true;
+    }
     
-    time2 = minutes == 60? 0 : time2;
-   
-    clock.text(`${hour}:${minutes}:${seconds}`);
-    time++;
-    time2++;
+    if (flag_seconds) {
+        if (seconds == 0) {
+            minutes++;
+            flag_seconds = false;
+        }
+    }
+
+    minutes = minutes % 60;
+    if (minutes == 1) {
+        flag_minutes = true;
+    }
+
+    if (flag_minutes) {
+        hour++;
+        flag_minutes = false;
+    }
+    
+    clock.text(`${hour}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`);
 }
 
 const pauseTimer = () => {
-    clearInterval(intervalId);
+    timer.stop();
+
     timer_button.text('Resume');
     timer_button.off('click', pauseTimer);
     timer_button.on('click', startTimer);
 
-    let time = getTime();
+    let end_time = getTime();
     let registro = registros[registros.length - 1];
-    registro.end = time;
+    registro.end = end_time;
 
-    let end_time = `<td>${registro.end}</td>`
-    $('.table-container tbody > tr:last-of-type').append(end_time)
+    let end_time_element = `<td>${registro.end}</td>`
+    $('.table-container tbody > tr:last-of-type').append(end_time_element)
     input_field.removeClass('off');
 }
 
@@ -63,7 +138,8 @@ const startTimer = () => {
         return null;
     } else {
         
-        intervalId = setInterval(updateCount, 1000);
+        timer.start();
+
         timer_button.text('Pause');
         timer_button.off('click', startTimer);
         timer_button.on('click', pauseTimer);
@@ -77,3 +153,4 @@ const startTimer = () => {
 }
 
 timer_button.on('click', startTimer);
+setInterval(updateCount, 100)
