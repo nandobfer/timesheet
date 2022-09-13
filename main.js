359722cdb793c85179173eff9af4b366af810d7e
nandobfer <nandobfer@yahoo.com.br>
2022-09-13 08:00:32 -0300

    fixed inactivity latency

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const createWindow = () => {
     webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
+        backgroundThrottling: false,
     },
     autoHideMenuBar: true,
   })
